ece79be59fadb73022f9e070128249a5ac3f542c
Antony <lexonimous@gmail.com>
2018-12-26 22:20:42 +0000

    Created the Mod Hook

---
+++ DistrictOverhaul/Source/ModHook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using ICities;
+namespace DistrictOverhaul.Source
+{
+    class ModHook : IUserMod
+    {
+        public string Name
+        {
+            get { return "Placeholder Name"; }
+        }
+
+        public string Description
+        {
+            get { return "A Placeholder Description for a placeholder Mod"; }
+        }
+                
+    }
+}
